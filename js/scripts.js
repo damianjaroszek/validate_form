7e6744f335db8d750be1ed4fcb3b4db98cce2450
damianjaroszek <deejay.8lm@gmail.com>
2021-06-06 22:45:30 +0200

    push data erros to array and show it on document like a li

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,58 @@
-(function() {
+// (function() {
 
 var form = document.querySelector("#myForm"),
     fields = form.querySelectorAll("[data-error]");
 
-})();
+
+
+function isEmpty(fields){
+    //console.log(fields.value)
+if (fields.value !==''){
+    return true;
+}
+else{ 
+    return false;
+}
+}    
+
+var tableError = [];
+var textNodes = [];
+var liTable = [];
+
+form.addEventListener("submit", function(e){
+
+      
+    e.preventDefault();
+
+    for (var i=0; i<fields.length; i++){
+    if(isEmpty(fields[i])==false){
+        tableError.push(fields[i].dataset.error);
+    }
+    }
+
+
+for(var k=0; k<fields.length; k++){
+    
+    textNodes.push(document.createTextNode(tableError[k]));
+}
+ 
+
+for(var j=0; j<textNodes.length; j++){
+
+    console.log(textNodes[j]);
+    var li = document.createElement("li");
+
+    li.appendChild(document.createTextNode(textNodes[j].textContent));
+    form.appendChild(li);
+}
+ 
+tableError.length=0;
+textNodes.length=0;
+liTable.length=0;
+
+}, false)
+
+
+
+
+// })();
