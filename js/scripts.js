5a2c4e713c57c5a385edbac7994a1a6d107cffdc
damianjaroszek <deejay.8lm@gmail.com>
2021-06-17 21:37:00 +0200

    another solution

--- js/scripts.js
+++ js/scripts.js
@@ -1,70 +1,67 @@
 // (function() {
-
+    
 var form = document.querySelector("#myForm"),
     fields = form.querySelectorAll("[data-error]");
 
+    form.addEventListener("submit", function(e){
+        e.preventDefault();
+
+        var errors = [];
+
+        function isEmpty(field){
+            if (field.value.length>0){
+                return false;
+            }else{
+                return true;
+            }
+        }
+
+        function isEmailCorrect(field){
+            if (field.value.indexOf('@')!=-1 && field.value.indexOf('.')!=-1 && !isEmpty(field)){
+                return false;
+            }
+            else{
+                return true;
+            }
+        }
+
+        function atLeast(field, min){
+            if (field.value.length>=min){
+                return false;    
+            }
+            else {
+                return true;
+            }
+        }
+
+        for(var i=0; i<fields.length; i++){
+           // console.log(fields[i].dataset.error);
+           var field = fields[i];
+
+           if (field.type==="text" || field.type==="select-one"){ 
+                if (isEmpty(field)){
+                        errors.push(field.dataset.error);
+                }
+            
+            }
+
+            if (field.type==="email"){
+                if (/*isEmpty(field) ||*/ isEmailCorrect(field)){
+                    errors.push(field.dataset.error);
+                }
+            }
 
 
-function isEmpty(fields){
-    //console.log(fields.value)
-if (fields.value !==''){
-    return true;
-}
-else{ 
-    return false;
-}
-}    
+            if (field.type==="textarea"){
+                if (atLeast(field, 3)){
+                    errors.push(field.dataset.error);
+                }
+            }
+        }
 
-var tableError = [];
-var textNodes = [];
-var liTable = [];
+        console.log(errors);
 
-form.addEventListener("submit", function(e){
-
-      
-    e.preventDefault();
-
-    for (var i=0; i<fields.length; i++){
-    if(isEmpty(fields[i])==false){
-        tableError.push(fields[i].dataset.error);
-    }
-    }
+        
 
 
-for(var k=0; k<fields.length; k++){
-
-    
-    textNodes.push(document.createTextNode(tableError[k]));
-    
-}
-
-var li;
-
-for(var j=0; j<textNodes.length; j++){
-
-    //console.log(textNodes[j]);
-    li = document.createElement("li");
-    
-    var content = textNodes[j].textContent;
-    if (content!='undefined'){
-    li.appendChild(document.createTextNode(content));
-   form.appendChild(li);
-   
-}
-
-//console.log(li);
-
-}
-
- 
-tableError.length=0;
-textNodes.length=0;
-liTable.length=0;
-
-
-}, false)
-
-
-
-
-// })();
+    }, false);
